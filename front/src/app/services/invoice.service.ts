ab0658a87c49b8a9d22874297628f0bae5f8bc52
nicolasmachut <machut.nicolas@gmail.com>
2019-04-03 19:30:36 -0400

    generate invoice pdf with jasper

--- front/src/app/services/invoice.service.ts
+++ front/src/app/services/invoice.service.ts
@@ -7,10 +7,6 @@ export class InvoiceService {
 
   constructor(private httpClient: HttpClient) { }
 
-  generateInvoice(start: Date, end: Date, customer: Customer) {
-    return this.httpClient.get('/api/invoice?customerId=' + customer.id + '&start=' + start + '&end=' + end, {responseType: 'text'});
-  }
-
   downloadInvoice(start: Date, end: Date, customer: Customer) {
     return this.httpClient.get('/api/invoice/pdf?customerId=' + customer.id + '&start=' + start + '&end=' + end, {responseType: 'blob'});
   }
